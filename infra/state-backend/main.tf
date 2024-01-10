2dd7e64e54a0559fd01d9512cfba96836240e494
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-10 12:46:39 -0300

    fixed workflow  path

--- infra/state-backend/main.tf
+++ infra/state-backend/main.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
  region = "us-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "kaio-lab-terraform-state"
+    key    = "state/terraform.tfstate"
+    region = "us-west-2"
+    encrypt = true
+    kms_key_id = "alias/terraform-bucket-key"
+  }
+}
