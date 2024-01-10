0c91d0861e65ec2ab0b2b054ccafe5f3837fb0c1
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-10 13:00:31 -0300

    added backend config to main state

--- infra/state-backend/main.tf
+++ infra/state-backend/main.tf
@@ -10,13 +10,3 @@ terraform {
 provider "aws" {
  region = "us-west-2"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "kaio-lab-terraform-state"
-    key    = "state/terraform.tfstate"
-    region = "us-west-2"
-    encrypt = true
-    kms_key_id = "alias/terraform-bucket-key"
-  }
-}
