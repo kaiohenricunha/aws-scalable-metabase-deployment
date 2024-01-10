f8f7252a5ff85fa17ef5bb96eabc1053bcb2291e
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-10 15:34:18 -0300

    change vpc module

--- infra/main.tf
+++ infra/main.tf
@@ -69,7 +69,6 @@ resource "aws_s3_bucket_public_access_block" "block" {
  restrict_public_buckets = true
 }
 
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.3"
@@ -77,7 +76,7 @@ module "vpc" {
   name = "lab-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["us-west-2a", "us-west-2b"]
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
   public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
 
@@ -92,3 +91,61 @@ module "vpc" {
     Environment = "lab"
   }
 }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "18.29.0"
+
+#   cluster_name    = "my-eks"
+#   cluster_version = "1.23"
+
+#   cluster_endpoint_private_access = true
+#   cluster_endpoint_public_access  = true
+
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+
+#   enable_irsa = true
+
+#   eks_managed_node_group_defaults = {
+#     disk_size = 50
+#   }
+
+#   eks_managed_node_groups = {
+#     general = {
+#       desired_size = 1
+#       min_size     = 1
+#       max_size     = 10
+
+#       labels = {
+#         role = "general"
+#       }
+
+#       instance_types = ["t3.small"]
+#       capacity_type  = "ON_DEMAND"
+#     }
+
+#     spot = {
+#       desired_size = 1
+#       min_size     = 1
+#       max_size     = 10
+
+#       labels = {
+#         role = "spot"
+#       }
+
+#       taints = [{
+#         key    = "market"
+#         value  = "spot"
+#         effect = "NO_SCHEDULE"
+#       }]
+
+#       instance_types = ["t3.micro"]
+#       capacity_type  = "SPOT"
+#     }
+#   }
+
+#   tags = {
+#     Environment = "staging"
+#   }
+# }
