d0c60c287b7e6b28db688df18812acd608938a9d
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-10 17:33:30 -0300

    apply eks

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,11 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.6.0"
+      version = ">= 2.6"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57"
     }
   }
 
@@ -141,86 +145,38 @@ module "eks" {
     }
   }
 
+  # Shown just for connection between cluster and Karpenter sub-module below
+  manage_aws_auth_configmap = true
+  aws_auth_roles = [
+    # We need to add in the Karpenter node IAM role for nodes launched by Karpenter
+    {
+      rolearn  = module.karpenter.role_arn
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups = [
+        "system:bootstrappers",
+        "system:nodes",
+      ]
+    },
+  ]
+
   tags = {
     Environment = "lab"
   }
 }
 
-module "allow_eks_access_iam_policy" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "5.3.1"
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
 
-  name          = "allow-eks-access"
-  create_policy = true
+  cluster_name = module.eks.cluster_name
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "eks:DescribeCluster",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
+  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
+  irsa_namespace_service_accounts = ["karpenter:karpenter"]
 
-module "eks_admins_iam_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "5.3.1"
+  create_iam_role = false
+  iam_role_arn    = module.eks.eks_managed_node_groups["initial"].iam_role_arn
 
-  role_name         = "eks-admin"
-  create_role       = true
-  role_requires_mfa = false
-
-  custom_role_policy_arns = [module.allow_eks_access_iam_policy.arn]
-
-  trusted_role_arns = [
-    "arn:aws:iam::${module.vpc.vpc_owner_id}:root"
-  ]
-}
-
-module "user1_iam_user" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
-  version = "5.3.1"
-
-  name                          = "user1"
-  create_iam_access_key         = false
-  create_iam_user_login_profile = false
-
-  force_destroy = true
-}
-
-module "allow_assume_eks_admins_iam_policy" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "5.3.1"
-
-  name          = "allow-assume-eks-admin-iam-role"
-  create_policy = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:AssumeRole",
-        ]
-        Effect   = "Allow"
-        Resource = module.eks_admins_iam_role.iam_role_arn
-      },
-    ]
-  })
-}
-
-module "eks_admins_iam_group" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-  version = "5.3.1"
-
-  name                              = "eks-admin"
-  attach_iam_self_management_policy = false
-  create_group                      = true
-  group_users                       = [module.user1_iam_user.iam_user_name]
-  custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
+  tags = {
+    Environment = "lab"
+    Terraform   = "true"
+  }
 }
