bc53c54c59b326774770ffa2c954e8759561041f
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-10 20:27:41 -0300

    change to fargate

--- infra/main.tf
+++ infra/main.tf
@@ -150,10 +150,10 @@ module "vpc" {
   name = "lab-vpc"
   cidr = local.vpc_cidr
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.128.0/19"]
+  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19", "10.0.160.0/19"]
+  intra_subnets = ["10.0.192.0/19", "10.0.224.0/19"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
