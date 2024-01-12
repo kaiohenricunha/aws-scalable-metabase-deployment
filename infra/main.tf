34e12544fb436ada42ba83a72f996341c7b268b8
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-12 00:08:39 -0300

    expose metabase

--- infra/main.tf
+++ infra/main.tf
@@ -573,3 +573,45 @@ resource "helm_release" "aws_load_balancer_controller" {
     value = aws_iam_role.aws_load_balancer_controller_role.arn
   }
 }
+
+################################################################################
+# Route53
+################################################################################
+
+resource "aws_route53_zone" "main" {
+  name = "metabasekaiolab.com"
+}
+
+resource "aws_route53_record" "metabase_lb" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "metabasekaiolab.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["k8s-metabase-metabase-30bdf9ccdf-836798141.us-west-2.elb.amazonaws.com"]
+}
+
+################################################################################
+# ACM: TLS Configuration
+################################################################################
+
+resource "aws_acm_certificate" "metabase_cert" {
+  domain_name       = "metabasekaiolab.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "metabase_cert" {
+  certificate_arn         = aws_acm_certificate.metabase_cert.arn
+  validation_record_fqdns = [aws_route53_record.metabase_cert_validation.fqdn]
+}
+
+resource "aws_route53_record" "metabase_cert_validation" {
+  name    = aws_acm_certificate.metabase_cert.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.metabase_cert.domain_validation_options[0].resource_record_type
+  zone_id = aws_route53_zone.main.zone_id
+  records = [aws_acm_certificate.metabase_cert.domain_validation_options[0].resource_record_value]
+  ttl     = 60
+}
