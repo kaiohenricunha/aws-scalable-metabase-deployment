8cc252c53afc5afc7e656a9a79baef4e46441611
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-13 14:28:59 -0300

    apply eks on fargate and karpenter

--- environments/lab/providers.tf
+++ environments/lab/providers.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.6"
