93a7bb0d18bfdf7717e638cf1c5e5704a000e59a
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-13 16:07:00 -0300

    apply eks on fargate and karpenter

--- environments/lab/outputs.tf
+++ environments/lab/outputs.tf
@@ -1,20 +1,19 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+  value       = module.lab_vpc.vpc_id
 }
 
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+  value       = module.lab_vpc.private_subnets
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
+  value       = module.lab_vpc.public_subnets
 }
 
 output "intra_subnets" {
   description = "List of IDs of intra subnets"
-  value       = module.vpc.intra_subnets
+  value       = module.lab_vpc.intra_subnets
 }
-
