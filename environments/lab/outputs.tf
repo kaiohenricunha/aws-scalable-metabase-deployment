47688c3113c015c8881218d628f82383eebb2999
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-13 15:08:32 -0300

    apply eks on fargate and karpenter

--- environments/lab/outputs.tf
+++ environments/lab/outputs.tf
@@ -1,35 +1,35 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+output "lab_vpc_id" {
+  description = "The ID of the lab_vpc"
+  value       = module.lab_vpc.lab_vpc_id
 }
 
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+  value       = module.lab_vpc.private_subnets
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
+  value       = module.lab_vpc.public_subnets
 }
 
 output "intra_subnets" {
   description = "List of IDs of intra subnets"
-  value       = module.vpc.intra_subnets
+  value       = module.lab_vpc.intra_subnets
 }
 
 output "cluster_id" {
-  description = "The EKS cluster ID."
-  value       = module.eks.cluster_id
+  description = "The eks_fargate_karpenter cluster ID."
+  value       = module.eks_fargate_karpenter.cluster_id
 }
 
 output "cluster_arn" {
-  description = "The EKS cluster ARN."
-  value       = module.eks.cluster_arn
+  description = "The eks_fargate_karpenter cluster ARN."
+  value       = module.eks_fargate_karpenter.cluster_arn
 }
 
 # Karpenter specific outputs
 output "karpenter_iam_role_arn" {
   description = "The IAM role ARN used by Karpenter."
-  value       = module.karpenter.role_arn
+  value       = module.eks_fargate_karpenter.role_arn
 }
--- environments/lab/providers.tf
+++ environments/lab/providers.tf
@@ -33,27 +33,27 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
+  host                   = module.eks_fargate_karpenter.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args = ["eks", "get-token", "--cluster-name", module.eks_fargate_karpenter.cluster_name]
   }
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    cluster_ca_certificate = base64decode(module.eks_fargate_karpenter.cluster_certificate_authority_data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args = ["eks", "get-token", "--cluster-name", module.eks_fargate_karpenter.cluster_name]
     }
   }
 }
@@ -62,13 +62,13 @@ provider "kubectl" {
   alias = "gavinbunney"
   apply_retry_count      = 5
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks_fargate_karpenter.cluster_certificate_authority_data)
   load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args = ["eks", "get-token", "--cluster-name", module.eks_fargate_karpenter.cluster_name]
   }
 }
