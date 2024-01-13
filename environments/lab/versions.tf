78d94e6c767daab7436296ffc4269fd5d7ed1698
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-13 13:57:02 -0300

    apply eks on fargate and karpenter

--- environments/lab/versions.tf
+++ environments/lab/versions.tf
@@ -6,14 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.31"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.6"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.0"
