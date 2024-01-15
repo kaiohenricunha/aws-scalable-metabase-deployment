37e29e52a4dca6af8b185cf3178a4e82bcdb957b
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-15 12:05:18 -0300

    update cidr blocks

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -6,9 +6,9 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.128.0/19"]
-  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19", "10.0.160.0/19"]
-  intra_subnets = ["10.0.192.0/19", "10.0.224.0/19"]
+  private_subnets = ["10.0.64.0/19", "10.0.96.0/19", "10.0.128.0/19"]  # Adjusted to avoid overlap
+  public_subnets  = ["10.0.160.0/19", "10.0.192.0/19", "10.0.224.0/19"]
+  intra_subnets = ["10.0.0.0/19", "10.0.32.0/19"]  # Placed after public and intra subnets to avoid conflicts
 
   enable_nat_gateway = true
   single_nat_gateway = true
