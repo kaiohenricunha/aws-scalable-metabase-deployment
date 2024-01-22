04ef152bb99f3b25aecb07abc2fe011bd5b2dd0f
kaiohenricunha <kaiohsdc@gmail.com>
2024-01-22 15:05:21 -0300

    update readme [skip ci]

--- infra/eks-fargate-karpenter/main.tf
+++ infra/eks-fargate-karpenter/main.tf
@@ -117,7 +117,7 @@ resource "helm_release" "karpenter" {
   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
   repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
-  version             = "v0.32.0"
+  version             = "v0.32.5"
 
   values = [
     <<-EOT
